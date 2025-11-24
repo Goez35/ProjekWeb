b543e293526def4970f01e36d334880b23a6fdf3
ilhamanam <ilhamanamrizaldi123@gmail.com>
2025-11-24 21:06:45 +0700

    koneksi

--- Projek/backend/config/db.php
+++ Projek/backend/config/db.php
@@ -1,13 +1,13 @@
 <?php
-// config/db.php
-$DB_HOST = '127.0.0.1';
-$DB_USER = 'root';
-$DB_PASS = '';
-$DB_NAME = 'quizdb';
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "quizdb";
 
-$mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
-if ($mysqli->connect_errno) {
-    http_response_code(500);
-    die(json_encode(['error' => 'Koneksi database gagal']));
+$koneksi = new mysqli($host, $user, $pass, $db);
+
+if ($koneksi->connect_error) {
+    die("Koneksi gagal: " . $koneksi->connect_error);
 }
-$mysqli->set_charset('utf8mb4');
+
+?>
