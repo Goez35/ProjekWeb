bfa9198a11dcff0ed474a965fa67b0e9d7de3caa
ilhamanam <ilhamanamrizaldi123@gmail.com>
2025-11-24 21:38:47 +0700

    ubah

--- Projek/backend/koneksi.php
+++ Projek/backend/koneksi.php
@@ -1,1 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "quizdb";
 
+$koneksi = new mysqli($host, $user, $pass, $db);
+
+if ($koneksi->connect_error) {
+    die("Koneksi gagal: " . $koneksi->connect_error);
+}
+?>
